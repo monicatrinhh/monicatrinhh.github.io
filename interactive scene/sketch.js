baf5b9b958f756533c6bbdbac8d7fff1a0f3ccac
monicatrinhh <trinhminhthao21193@gmail.com>
2021-09-24 12:08:05 -0600

    copy template

---
+++ interactive scene/sketch.js
@@ -0,0 +1,19 @@
+// Interactive Scene
+// Monica Trinh
+// September 20,2021
+//
+/* Extra for Experts:
+  + HTML/CSS banner
+  + Sound
+  + popping bubbles
+*/
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  rect(mouseX,mouseY,100,50);
+}
