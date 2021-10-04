5a191b72d15b7b85041f358989ee2f10be0b24d8
monicatrinhh <trinhminhthao21193@gmail.com>
2021-10-04 11:40:35 -0600

    jiggly bubble

--- bubble/sketch.js
+++ bubble/sketch.js
@@ -1,19 +1,54 @@
-// Interactive Scene
+// Bubble Demo
 // Monica Trinh
 // September 20,2021
-//
-/* Extra for Experts:
-  + HTML/CSS banner
-  + Sound
-  + popping bubbles
-*/
 
+let theBubble = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+ 
 }
 
 function draw() {
   background(220);
-  rect(mouseX,mouseY,100,50);
+  bubbleUp();
+  displayBubble();
+}
+
+function mousePressed(){
+  for (let i=0; i<5;i++){
+    spawnBubble();
+  }
+  
+}
+
+function spawnBubble(){
+  let bubble = {
+    x: mouseX,
+    y: mouseY,
+    radius: random(20,50),
+    dx: 0,
+    dy: -3,
+    theColor: color(random(255),random(255),random(255)),
+  }
+  theBubble.push(bubble);
+}
+
+function bubbleUp() {
+  for (let bubble of theBubble){
+    bubble.y += bubble.dy;
+
+    // jitter sideways
+    bubble.x += random(-5,5);
+  }
+  
+}
+
+function displayBubble(){
+
+  for (let bubble of theBubble){
+    noStroke();
+    circle(bubble.x, bubble.y, bubble.radius*2);
+    fill(bubble.theColor);
+  }
 }
