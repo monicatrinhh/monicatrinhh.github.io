526a3ee284a31d22800c2d065fe2f2d3692167f5
monicatrinhh <trinhminhthao21193@gmail.com>
2021-10-08 11:44:45 -0600

    add

---
+++ grid/sketch.js
@@ -0,0 +1,19 @@
+// Grid
+// Monica Trinh
+// Date
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  rect(mouseX,mouseY,100,50);
+}
+
+function createEmptyArray(rows,cols){
+  let grid = [];
+  
+}
