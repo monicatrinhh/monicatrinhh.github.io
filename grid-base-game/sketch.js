d09d4f63dcada1acfbe22ed8edb514a1c2eb6da8
monicatrinhh <trinhminhthao21193@gmail.com>
2021-10-19 11:47:56 -0600

    create index

---
+++ grid-base-game/sketch.js
@@ -0,0 +1,14 @@
+// Project Name
+// Monica Trinh
+// Date
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  rect(mouseX,mouseY,100,50);
+}
