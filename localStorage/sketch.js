f4d1324ec3feb4abbbbaaae7d3d3c51f25e13f96
monicatrinhh <trinhminhthao21193@gmail.com>
2021-11-25 11:21:34 -0600

    local storage

---
+++ localStorage/sketch.js
@@ -0,0 +1,14 @@
+// Local Storage
+// Monica Trinh
+// Nov 25th, 2021
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  rect(mouseX,mouseY,100,50);
+}
