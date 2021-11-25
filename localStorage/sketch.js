623be650664c53be82ba7a14a1edc305b841d8fa
monicatrinhh <trinhminhthao21193@gmail.com>
2021-11-25 12:11:00 -0600

    update display for player.js

--- localStorage/sketch.js
+++ localStorage/sketch.js
@@ -2,7 +2,7 @@
 // Monica Trinh
 // Nov 25th, 2021
 
-
+let clickCount = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -10,5 +10,17 @@ function setup() {
 
 function draw() {
   background(220);
-  rect(mouseX,mouseY,100,50);
+  textSize(42);
+  text(clickCount, width/2, height/2);
+
+  fill("red")
+  text(getItem("highest"),100,100);
+  
+}
+
+function mousePressed(){
+  clickCount++;
+  if (clickCount > getItem("highest")){
+    storeItem("highest", clickCount);
+  }
 }
